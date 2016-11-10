e506a57ec9e44a5b79efc69b263c86dd8d6593a6
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 21:25:16 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -5,7 +5,7 @@ layout: null
 $(document).ready(function(){
 	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
 	$("#typed").typed({
-		strings: ["Hi!", "I'm a student", "I'm a blogger", "I'm a developer", "I'm Viet Anh.<br>$ Nice to meet you!"],
+		strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.<br>$ Nice to meet you!"],
 		typeSpeed: 45
 	});
 	
