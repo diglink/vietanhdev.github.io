98c4462015291e81da44513191c33f46af4939a3
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 20:36:12 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -2,10 +2,19 @@
 layout: null
 ---
 {% include /js/typed.js %}
-function line2(){
+$(document).ready(function(){
+	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed1'></span><br>$ <span id='typed2'></span><br>$ <span id='typed3'></span><br>$ <span id='typed4'></span><br>$ <span id='typed5'></span><br></div></div>");
+	$("#typed1").typed({
+		strings: ["sTARTing.", "sTARTing...", "sTARTing....."],
+		typeSpeed: 20,
+		callback: function(){
+        	line2();
+      	}
+	});
+	function line2(){
 		$("#typed2").typed({
 			strings: ["Hi!"],
-			typeSpeed: 45
+			typeSpeed: 45,
 			callback: function(){
 	        	line3();
 	      	}
@@ -14,7 +23,7 @@ function line2(){
 	function line3(){
 	$("#typed3").typed({
 		strings: ["I'm a student"],
-		typeSpeed: 45
+		typeSpeed: 45,
 	 	callback: function(){
 	        	line4();
 	      	}
@@ -23,7 +32,7 @@ function line2(){
 	function line4(){
 	$("#typed4").typed({
 		strings: ["I'm a blogger"],
-		typeSpeed: 45
+		typeSpeed: 45,
 		callback: function(){
 	        	line5();
 	      	}
@@ -32,7 +41,7 @@ function line2(){
 	function line5(){
 	$("#typed5").typed({
 		strings: ["I'm a developer"],
-		typeSpeed: 45
+		typeSpeed: 45,
 		callback: function(){
 	        	line6();
 	      	}
@@ -44,13 +53,4 @@ function line2(){
 		typeSpeed: 45
 		});
 	}
-$(document).ready(function(){
-	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed1'></span><br>$ <span id='typed2'></span><br>$ <span id='typed3'></span><br>$ <span id='typed4'></span><br>$ <span id='typed5'></span><br></div></div>");
-	$("#typed1").typed({
-		strings: ["sTARTing.", "sTARTing...", "sTARTing....."],
-		typeSpeed: 20,
-		callback: function(){
-        	line2();
-      	}
-	});
 });
