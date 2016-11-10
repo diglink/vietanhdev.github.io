c1d9768367f08deb6261f6953c5efcd27bed664a
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 20:43:36 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -3,7 +3,7 @@ layout: null
 ---
 {% include /js/typed.js %}
 $(document).ready(function(){
-	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed1'></span><br>$ <span id='typed2'></span><br>$ <span id='typed3'></span><br>$ <span id='typed4'></span><br>$ <span id='typed5'></span><br></div></div>");
+	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed1'></div></div>");
 	$("#typed1").typed({
 		strings: ["sTARTing.", "sTARTing...", "sTARTing....."],
 		typeSpeed: 20,
@@ -12,6 +12,8 @@ $(document).ready(function(){
       	}
 	});
 	function line2(){
+		$(".typed-cursor").remove();
+		$(".page-heading").append("<br>$ <span id='typed2'></span>");
 		$("#typed2").typed({
 			strings: ["Hi!"],
 			typeSpeed: 45,
@@ -21,16 +23,20 @@ $(document).ready(function(){
 		});
 	}
 	function line3(){
-	$("#typed3").typed({
-		strings: ["I'm a student"],
-		typeSpeed: 45,
-	 	callback: function(){
-	        	line4();
-	      	}
+		$(".typed-cursor").remove();
+		$(".page-heading").append("<br>$ <span id='typed3'></span>");
+		$("#typed3").typed({
+			strings: ["I'm a student"],
+			typeSpeed: 45,
+		 	callback: function(){
+		        	line4();
+		      	}
 		});
 	}
 	function line4(){
-	$("#typed4").typed({
+		$(".typed-cursor").remove();
+		$(".page-heading").append("<br>$ <span id='typed4'></span>");
+		$("#typed4").typed({
 		strings: ["I'm a blogger"],
 		typeSpeed: 45,
 		callback: function(){
@@ -39,7 +45,9 @@ $(document).ready(function(){
 		});
 	}
 	function line5(){
-	$("#typed5").typed({
+		$(".typed-cursor").remove();
+		$(".page-heading").append("<br>$ <span id='typed5'></span>");
+		$("#typed5").typed({
 		strings: ["I'm a developer"],
 		typeSpeed: 45,
 		callback: function(){
@@ -48,7 +56,9 @@ $(document).ready(function(){
 		});
 	}
 	function line6(){
-	$("#typed5").typed({
+		$(".typed-cursor").remove();
+		$(".page-heading").append("<br>$ <span id='typed6'></span>");
+		$("#typed6").typed({
 		strings: ["I'm Viet Anh. Nice to meet you!"],
 		typeSpeed: 45
 		});
