6d3963bffb182826553c95cb4d221089377cf138
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 22:14:36 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -4,15 +4,21 @@ layout: null
 {% include /js/typed.js %}
 $(document).ready(function(){
 	var w = window.innerWidth;
-	if (w > 700) {
+	function helloWorld(){
 		// Set CSS for .page-heading
-		$(".page-heading").css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"});
-		setInterval(function(){
-		    $(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
-			$("#typed").typed({
-				strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.<br>$ Nice to meet you!"],
-				typeSpeed: 45
-			});
-		}, 3000);
+		$(".page-heading").css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"})
+		$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
+		$("#typed").typed({
+			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.<br>$ Nice to meet you!"],
+			typeSpeed: 45,
+			callback: function(){
+		    	setTimeout(function(){
+			        helloWorld();
+			    }, 3000);
+		    }
+		});
+	}
+	if (w > 700) {
+		helloWorld();
 	}
 });
