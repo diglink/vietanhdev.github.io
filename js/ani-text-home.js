eef9c550caad0bbd431fa70ab8651b6ba96de488
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 22:04:22 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -5,13 +5,14 @@ layout: null
 $(document).ready(function(){
 	var w = window.innerWidth;
 	if (w > 700) {
-
 		// Set CSS for .page-heading
-		$(".page-heading").css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"})
-		$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
-		$("#typed").typed({
-			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.<br>$ Nice to meet you!"],
-			typeSpeed: 45
-		});
+		$(".page-heading").css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"});
+		setInterval(function(){
+		    $(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
+			$("#typed").typed({
+				strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.<br>$ Nice to meet you!"],
+				typeSpeed: 45
+			});
+		}, 3000);
 	}
 });
