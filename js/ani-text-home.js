0f36a7516f5d98555f6910e538221d91e68fc733
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 16:54:01 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -3,7 +3,7 @@ layout: null
 ---
 {% include /js/typed.js %}
 $(function(){
-	$(".page-heading").typed({
+	$("").typed({
 		strings: ["I'm a student.", "I'm a blogger.", "I'm a developer.", "Hi! I'm Viet Anh."],
 		typeSpeed: 30,
 		callback: function(){
