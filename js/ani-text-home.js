20f0a442619ccce55b67db036ce657f9d48073f5
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-10 21:16:09 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -3,52 +3,10 @@ layout: null
 ---
 {% include /js/typed.js %}
 $(document).ready(function(){
-	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ downloading<span id='typed1'></div></div>");
-	$("#typed1").typed({
-		strings: [".", "..", "..."],
-		typeSpeed: 20,
-		callback: function(){
-        	line2().delay(1000).line3().delay(1000).line4().delay(1000).line5().delay(1000).line6();
-      	}
+	$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>Terminal</span</div><div class='text-body'>$ <span id='typed'></div></div>");
+	$("#typed").typed({
+		strings: ["Hi!", "I'm a student", "I'm a blogger", "I'm a developer", "I'm Viet Anh.  Nice to meet you!"],
+		typeSpeed: 45
 	});
-	function line2(){
-		$(".typed-cursor").remove();
-		$("#typed1").append("<br>$ <span id='typed2'></span>");
-		$("#typed2").typed({
-			strings: ["Hi!"],
-			typeSpeed: 60
-		});
-	}
-	function line3(){
-		$(".typed-cursor").remove();
-		$("#typed2").append("<br>$ <span id='typed3'></span>");
-		$("#typed3").typed({
-			strings: ["I'm a student"],
-			typeSpeed: 60
-		});
-	}
-	function line4(){
-		$(".typed-cursor").remove();
-		$("#typed3").append("<br>$ <span id='typed4'></span>");
-		$("#typed4").typed({
-			strings: ["I'm a blogger"],
-			typeSpeed: 60
-		});
-	}
-	function line5(){
-		$(".typed-cursor").remove();
-		$("#typed4").append("<br>$ <span id='typed5'></span>");
-		$("#typed5").typed({
-			strings: ["I'm a developer"],
-			typeSpeed: 60
-		});
-	}
-	function line6(){
-		$(".typed-cursor").remove();
-		$("#typed5").append("<br>$ <span id='typed6'></span>");
-		$("#typed6").typed({
-			strings: ["I'm Viet Anh. Nice to meet you!"],
-			typeSpeed: 60
-		});
-	}
+	
 });
