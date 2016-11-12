97956ff43b26ba86ec6d60025c35fe84dd27f9db
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-11-12 07:54:55 +0700

    update web

--- js/ani-text-home.js
+++ js/ani-text-home.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 	var w = window.innerWidth;
 	function helloWorld(){
 		// Set CSS for .page-heading
-		$(".page-heading").css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"})
-		$(".page-heading").html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>vietanh@Xmachine</span></div><div class='text-body'>$ <span id='typed'></span></div></div>");
+		$(".page-heading").eq(1).remove();
+		$(".page-heading").eq(0).css({"margin":"0", "padding-top":"15px", "padding-bottom":"15px"})
+		$(".page-heading").eq(0).html("<div class='text-editor-wrap'><div class='title-bar'><span class='title'>vietanh@Xmachine</span></div><div class='text-body'>$ <span id='typed'></span></div></div>");
 		$("#typed").typed({
 			strings: ["Hi!", "I'm a <b>student.</b>", "I'm a <b>blogger.</b>", "I'm a <b>developer.</b>", "I'm <b>Viet Anh.</b><br>$ Nice to meet you!"],
 			cursorChar: " ☕",
